9645321410dd896e0aa4924bb83298f7cc162b43
Ivan Clint A. Pabelona <ipabelona@gmail.com>
2015-03-25 19:28:07 +0800

    users page

--- application/views/pages/users/users_page.php
+++ application/views/pages/users/users_page.php
@@ -1,6 +1,7 @@
 <div class="container">
   <div class="content">
-    <div class="row" style="height: 600px;">
+
+<div class="main-container-admin">
     	<p class="bg-success text-center" style="color: #000; padding: 10px; margin-top: 10%;"><strong>Users Page</strong> <br /><br /><a href="<?php print site_url('users/logout'); ?>" class="btn btn-danger">Logout</a></p>
     </div>
   </div>
